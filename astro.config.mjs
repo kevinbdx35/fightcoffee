22eadf14fefa288fe8c6d8c8f54aa4937de64f9c
Kevin <k.bardoux@orange.fr>
2026-03-25 09:54:52 +0100

    Add GitHub Actions deploy workflow + set site base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kevinbdx35.github.io',
+  base: '/fightcoffee',
+});
